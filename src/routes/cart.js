e85e12c937c8c2442c94f213d70ea2cc74dbc799
Wesley <wesley.santos140300@gmail.com>
2024-05-17 16:26:11 -0300

    feat: create routes order items and new model order delivery status

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -99,5 +99,77 @@ router.get("/get-cart-open-with-items-cart/:idUser", unauthorized, async(req, re
 });
 
 
+router.get("/get-cart-by-id/:id", unauthorized, async(req, res) => {
+    const id = req.params.id;
+    try {
+        const results = await Cart.aggregate([{
+                $match: {
+                    _id: new ObjectId(id),
+                }
+            },
+            {
+                $lookup: {
+                    from: "itens_carrinho",
+                    let: { carrinho_id: "$_id" },
+                    pipeline: [{
+                        $match: {
+                            $expr: {
+                                $eq: ["$$carrinho_id", "$carrinho_id"]
+                            }
+                        }
+                    }],
+                    as: "itens_carrinho",
+                }
+            },
+            {
+                $lookup: {
+                    from: "endereco_usuario",
+                    let: { endereco_usuario_id: "$usuario_id" },
+                    pipeline: [{
+                        $match: {
+                            $expr: {
+                                $eq: ["$$endereco_usuario_id", "$usuario_id"]
+                            }
+                        }
+                    }],
+                    as: "endereco_usuario",
+                }
+            },
+            {
+                $project: {
+                    "_id": 1,
+                    "usuario_id": 1,
+                    "status_compra": 1,
+                    "valor_compra": 1,
+                    "valor_total_compra": 1,
+                    "taxa_fixa": 1,
+                    "valor_total_com_taxa": 1,
+                    "itens_carrinho._id": 1,
+                    "itens_carrinho.produto_id": 1,
+                    "itens_carrinho.quantidade": 1,
+                    "itens_carrinho.quantidade": 1,
+                    "itens_carrinho.preco_total": 1,
+                    "itens_carrinho.lista_ingredientes.id": 1,
+                    "itens_carrinho.lista_ingredientes.preco": 1,
+                    "endereco_usuario._id": 1,
+                    "endereco_usuario.cidade": 1,
+                    "endereco_usuario.bairro": 1,
+                    "endereco_usuario.numero": 1,
+                    "endereco_usuario.complemento": 1,
+                }
+            }
+        ])
+
+        if (!results) {
+            res.status(404).send({ message: "Carrinho aberto não encontrado" });
+        } else {
+            res.status(200).send({ results: results });
+        }
+    } catch (error) {
+        console.error("Erro ao buscar Carrinho em aberto por ID do usuário:", error);
+        res.status(500).send({ message: "Erro ao buscar Carrinho em aberto por ID do usuário" });
+    }
+});
+
 
 export default router;
