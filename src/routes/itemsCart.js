84ec0bbd9c7e66eb5dd316dfa2fb48e93d919193
Marilia <marilialloureiro@hotmail.com>
2024-05-18 14:19:28 -0300

    feat: Itens carrinho > ingredientes adicionais + produto

--- src/routes/itemsCart.js
+++ src/routes/itemsCart.js
@@ -8,5 +8,29 @@ const unauthorized = passport.authenticate('jwt', { session: false });
 
 const router = express.Router();
 
+router.post('/add-itemsCart', unauthorized, async (req, res) => {
+    const { carrinho_id, produto_id, lista_ingredientes, quantidade, preco_unitario } = req.body;
+    try {
+        const precoIngredientes = lista_ingredientes.reduce((total, ing) => total + ing.preco, 0);
+        const precoTotal = (preco_unitario + precoIngredientes) * quantidade;
+        // talvez não precise fazer a soma aqui, soma feita no front
+
+        const ItemsCart = new ItemsCart({
+            carrinho_id: new ObjectId(carrinho_id),
+            produto_id: new ObjectId(produto_id),
+            lista_ingredientes,
+            quantidade,
+            preco_unitario,
+            preco_total: precoTotal,
+        });
+
+        await ItemsCart.save();
+        res.status(201).send(cartItem);
+    } catch (error) {
+        console.error('Erro ao adicionar item ao carrinho:', error);
+        res.status(500).send({ message: 'Erro ao adicionar item ao carrinho' });
+    }
+});
+
 
 export default router;
