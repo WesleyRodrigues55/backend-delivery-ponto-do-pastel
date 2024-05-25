6e877dd6868143a2a12187cb00eb02972c9ae66b
Marilia <marilialloureiro@hotmail.com>
2024-05-25 14:09:42 -0300

    feat: api itemsCart

--- src/routes/itemsCart.js
+++ src/routes/itemsCart.js
@@ -1,36 +1,34 @@
 import express from "express";
-import "../config/db.js";
-import { ObjectId } from "mongodb";
-import ItemsCart from "../model/ItemsCart.js";
 import passport from 'passport';
+import "../config/db.js";
+import ItemsCart from "../model/ItemsCart.js";
 
 const unauthorized = passport.authenticate('jwt', { session: false });
 
 const router = express.Router();
 
-router.post('/add-itemsCart', unauthorized, async (req, res) => {
-    const { carrinho_id, produto_id, lista_ingredientes, quantidade, preco_unitario } = req.body;
+router.post('/add-items-cart', unauthorized, async (req, res) => {
+    const query = req.body;
     try {
-        const precoIngredientes = lista_ingredientes.reduce((total, ing) => total + ing.preco, 0);
-        const precoTotal = (preco_unitario + precoIngredientes) * quantidade;
-        // talvez não precise fazer a soma aqui, soma feita no front
+        const newItemCart = new ItemsCart(query);
+        const results = await newItemCart.save();
 
-        const ItemsCart = new ItemsCart({
-            carrinho_id: new ObjectId(carrinho_id),
-            produto_id: new ObjectId(produto_id),
-            lista_ingredientes,
-            quantidade,
-            preco_unitario,
-            preco_total: precoTotal,
-        });
-
-        await ItemsCart.save();
-        res.status(201).send(cartItem);
+        //66490773f91d60d7834e08c2
+        res.status(200).send({ results: "Item adicionado ao carrinho" });
     } catch (error) {
         console.error('Erro ao adicionar item ao carrinho:', error);
         res.status(500).send({ message: 'Erro ao adicionar item ao carrinho' });
     }
 });
 
+router.get('/get-items-cart', unauthorized, async (req, res) => {
+    try {
+        const results = await ItemsCart.find({});
+        res.status(200).send({ results: results });
+    } catch (error) {
+        console.error("Erro ao buscar itens do carrinho", error);
+        res.status(500).send({ message: "Erro ao buscar itens do carrinho" });
+    }
+});
 
 export default router;
